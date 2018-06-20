5d4281690e81f168dcc8e690690a7658b491f80d
odinthenerd <odinthenerd@gmail.com>
2018-06-20 10:19:29 +0200

    bugfix

--- test/filter_test.hpp
+++ test/filter_test.hpp
@@ -7,7 +7,7 @@ namespace filter_test {
     using namespace boost::tmp;
 
     template<typename T>
-    using is_even = tmp::bool_<(T::value % 2 == 0)>;
+    using is_even = bool_<(T::value % 2 == 0)>;
     int run() {
         list_<int,int>{} = call_<filter_<is_<int>>,bool,char,int,int,bool>{};
 
@@ -15,9 +15,9 @@ namespace filter_test {
 
         //fusion
         std::pair<int,int>{} = pack_(false,'a',1,2,true) >>= filter_<is_<int>>{} | [](auto l, auto r){ return std::make_pair(l,r);};
-        int{} = pack_(false,'a',1,true) >>= filter_<is_<int>>{} | [](auto l){ return l; };
+        1 == (pack_(false,'a',1,true) >>= filter_<is_<int>>{} | [](auto l){ return l; });
         pack_() = pack_() >>= filter_<lift_<is_even>>{};
-        
+
         return 0;
     }
 }
